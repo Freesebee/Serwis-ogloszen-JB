c8d5cb771152701479b6b5a535645a347b9e2433
konra <konrad.augustynowicz11@gmail.com>
2022-04-25 21:07:34 +0200

    not initial but almost initial commit

---
+++ src/main/java/com/example/backniznes/BackniznesApplication.java
@@ -0,0 +1,13 @@
+package com.example.backniznes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackniznesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackniznesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backniznes/BackniznesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backniznes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackniznesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
